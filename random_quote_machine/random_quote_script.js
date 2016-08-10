f4f69aa78e3cd8b3b661731648f6ca5428a5e78e
svmi3195 <target1983@gmail.com>
2016-08-10 16:16:19 +0300

    Add files via upload

---
+++ random_quote_machine/random_quote_script.js
@@ -0,0 +1,22 @@
+﻿function randQuote(){
+
+$.ajax({
+            url: 'http://quotes.stormconsultancy.co.uk/random.json?callback=my_method',
+            type: 'GET',
+            dataType: 'jsonp',
+            error: function(xhr, status, error) {
+                $('#quote').text("Error occured while trying to get the quote, sorry!");
+            },
+            success: function(json) {
+                $('#quote').text(json.quote);
+		$('#author').text(json.author);
+            }
+
+
+ 
+});
+
+
+
+};
+
