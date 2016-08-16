2462f83d3c967c530e938a3d3b91e9c7b7cd4a0d
svmi3195 <target1983@gmail.com>
2016-08-16 11:32:16 +0300

    Add files via upload

--- local_weather_app/local_weather_script.js
+++ local_weather_app/local_weather_script.js
@@ -1,1 +1,9 @@
-﻿
+﻿function go(){
+ 
+$.getJSON("http://ip-api.com/json/?callback=?",function(result){   $('#location').text(result.city + ", " + result.countryCode);   
+getWeather(result.city + "," + result.countryCode);                   
+})};
+
+function getWeather(loc){
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + loc + "&APPID=d6a50d743e455ef9a573d33c339c225b&units=metric",function(data){   $('#weather').text(Math.round(data.main.temp) + " °C " + data.weather[0].main);});
+    };
