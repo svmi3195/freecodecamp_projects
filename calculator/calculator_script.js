f83f9f808e645f9f03f38911ef426a52dd504b70
svmi3195 <target1983@gmail.com>
2016-10-26 16:03:15 +0300

    basic features working

--- calculator/calculator_script.js
+++ calculator/calculator_script.js
@@ -1,1 +1,54 @@
-﻿
+﻿var leftOp = '';
+var rightOp = '';
+var sign = '';
+var result = '';
+
+var calculate = function (left, op, right){
+  left = parseFloat(left);
+  right = parseFloat(right);
+    
+  switch(op){
+    case '+': return left + right;
+    case '-': return left - right;
+    case '*': return left * right;
+    case '/': return left / right;
+  }
+}
+
+$(document).ready(go());
+
+function go(){
+$('button').on('click', function(){
+  var pressedBtn = $(this).text();
+  $('.logs').append(pressedBtn);
+  
+  if(pressedBtn === 'C'){
+    leftOp = '';
+    rightOp = '';
+    $('.current').text(rightOp);
+    $('.logs').text(rightOp);
+  }else if (pressedBtn === '='){
+    result = calculate(leftOp, sign, rightOp);
+    $('.current').text(result);
+    $('.logs').append(result);
+    leftOp = '';
+    rightOp = result;
+  }else if (pressedBtn === '+' || pressedBtn === '-' || pressedBtn === '*'  || pressedBtn === '/'){
+    if(leftOp !== ''){
+      leftOp = calculate(leftOp, sign, rightOp);
+    }else{
+      leftOp = rightOp;
+    }    
+    rightOp = '';
+    sign = pressedBtn;
+    $('.current').text(rightOp);
+  }else{
+    if(rightOp === result){
+      rightOp = '';
+    }
+    rightOp += pressedBtn;
+    $('.current').text(rightOp);   
+  }
+  
+})
+}
