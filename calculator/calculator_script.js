a50f4698137a837547ece37978ba1556a5a446bc
svmi3195 <target1983@gmail.com>
2016-10-26 17:54:55 +0300

    changed if-else chain to switch

--- calculator/calculator_script.js
+++ calculator/calculator_script.js
@@ -19,51 +19,64 @@ $(document).ready(go());
 
 function go(){	
 	
-	
-	
 $('button').on('click', function(){
 	
-	
-	
   var pressedBtn = $(this).text();
+  
+  //clear logs from welcome and add pressed value
+  if($('.logs').text() === "Let's do math!"){
+	  $('.logs').text('');
+  }  
   $('.logs').append(pressedBtn);
   
-  if(pressedBtn === 'C'){
-    leftOp = '';
-    rightOp = '';
-    $('.current').text(rightOp);
-    $('.logs').text(rightOp);
-  }else if (pressedBtn === '='){
-    result = calculate(leftOp, sign, rightOp);
-    $('.current').text(result);
-    $('.logs').append(result);
-    leftOp = '';
-    rightOp = result;
-  }else if (pressedBtn === '+' || pressedBtn === '-' || pressedBtn === '*'  || pressedBtn === '/'){
-    if(leftOp !== ''){
-      leftOp = calculate(leftOp, sign, rightOp);
-    }else{
-      leftOp = rightOp;
-    }    
-    rightOp = '';
-    sign = pressedBtn;
-    $('.current').text(rightOp);
-  }else{
-    if(rightOp === result){
-      rightOp = '';
-    }
-    rightOp += pressedBtn;
-    $('.current').text(rightOp);   
+  switch(pressedBtn){
+	case 'C':
+		leftOp = '';
+		rightOp = '';
+		$('.current').text(rightOp);
+		$('.logs').text(rightOp);
+		break;
+    case '=':
+		result = calculate(leftOp, sign, rightOp);
+		$('.current').text(result);
+		$('.logs').append(result);
+		leftOp = '';
+		rightOp = result;
+		break;
+    case '+':
+	case '-':
+	case '*':
+	case '/':
+		if(leftOp !== ''){
+		  leftOp = calculate(leftOp, sign, rightOp);
+		}else{
+		  leftOp = rightOp;
+		}    
+		rightOp = '';
+		sign = pressedBtn;
+		$('.current').text(rightOp);
+		break;
+    default:
+		if(rightOp === result){
+		  rightOp = '';
+		}
+		rightOp += pressedBtn;
+		$('.current').text(rightOp);   
+	    
   }
   
   
-  if(($('.current').text()).length > 14){
+
+  
+//handling screen overflow for main  
+	if(($('.current').text()).length > 14){
 		$('.current').text('Overflow!');
 		$('.logs').text('Number too long...');
 	}
 
+//handling screen overflow for logs
 	if(($('.logs').text()).length > 23){
-		reduced = $('.logs').text().slice(-23);
+		reduced = '...' + $('.logs').text().slice(-21);
 		$('.logs').text(reduced);
 	}
   
