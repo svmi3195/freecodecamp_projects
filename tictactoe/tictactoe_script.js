e40f3d19afe31ee6558e33788895edaaaff0debb
svmi3195 <target1983@gmail.com>
2016-10-28 16:13:39 +0300

    correct check for wins

--- tictactoe/tictactoe_script.js
+++ tictactoe/tictactoe_script.js
@@ -7,19 +7,27 @@ var ai = [];
 $(document).ready(go());
 
 function go(){
-$('button').on('click', function(){
-	var moveP = $(this).attr('id');
-	$('#' + moveP).html('X');
-	player.push(Number(moveP));
-	checkWin(player.sort());
-	available.splice(available.indexOf(Number(moveP)), 1);
-	aiMove();
+	$('button').on('click', function(){
+		var moveP = $(this).attr('id');
+		$('#' + moveP).html('X');
+		player.push(Number(moveP));
+		checkWin(player.sort(), 'Player');
+		available.splice(available.indexOf(Number(moveP)), 1);
+		aiMove();
 })}
 
-function checkWin(arr){
-	for(var i =0; i < winners.length; i++){
-		if(winners[i].toString() === arr.toString()){
-			alert('winner');
+function checkWin(arr, who){
+	for(var i = 0; i < winners.length; i++){
+		var check = 0;
+		for(var j = 0; j < 3; j++){
+			if(arr.includes(winners[i][j])){
+				check++;
+			}else{
+				blank = winners[i][j];
+			}
+		}
+		if(check === 3){
+			alert(who + ' wins!');
 		}
 	}
 }
@@ -28,6 +36,6 @@ function aiMove(){
 	var moveAI = available[0];
 	$('#' + moveAI).html('O');
 	ai.push(Number(moveAI));
-	checkWin(ai.sort());
+	checkWin(ai.sort(), 'AI');
 	available.splice(available.indexOf(Number(moveAI)), 1);
 }
