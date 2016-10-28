5be3b74458efae77848e567b850dda1c91583556
svmi3195 <target1983@gmail.com>
2016-10-28 14:19:09 +0300

    basic functionality with weak ai

--- tictactoe/tictactoe_script.js
+++ tictactoe/tictactoe_script.js
@@ -1,8 +1,33 @@
-﻿$(document).ready(go());
+var winners = [ [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+var available = [5,1,3,7,9,2,4,6,8];
+
+var player = [];
+var ai = [];
+
+$(document).ready(go());
 
 function go(){
-$('button').on('click', function(){	
-	var id = '#' + $(this).attr('id');
-	$(id).html('x');
-	
+$('button').on('click', function(){
+	var moveP = $(this).attr('id');
+	$('#' + moveP).html('X');
+	player.push(Number(moveP));
+	checkWin(player.sort());
+	available.splice(available.indexOf(Number(moveP)), 1);
+	aiMove();
 })}
+
+function checkWin(arr){
+	for(var i =0; i < winners.length; i++){
+		if(winners[i].toString() === arr.toString()){
+			alert('winner');
+		}
+	}
+}
+
+function aiMove(){
+	var moveAI = available[0];
+	$('#' + moveAI).html('O');
+	ai.push(Number(moveAI));
+	checkWin(ai.sort());
+	available.splice(available.indexOf(Number(moveAI)), 1);
+}
