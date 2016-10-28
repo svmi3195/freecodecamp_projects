c6294c61d53337fe365c60a13275df49aaa59790
svmi3195 <target1983@gmail.com>
2016-10-28 17:17:28 +0300

    better ai, still has bugs

--- tictactoe/tictactoe_script.js
+++ tictactoe/tictactoe_script.js
@@ -3,10 +3,12 @@ var available = [5,1,3,7,9,2,4,6,8];
 
 var player = [];
 var ai = [];
+var blank = null;
 
 $(document).ready(go());
 
 function go(){
+//player makes move by cliking button
 	$('button').on('click', function(){
 		var moveP = $(this).attr('id');
 		$('#' + moveP).html('X');
@@ -16,26 +18,40 @@ function go(){
 		aiMove();
 })}
 
-function checkWin(arr, who){
-	for(var i = 0; i < winners.length; i++){
-		var check = 0;
-		for(var j = 0; j < 3; j++){
-			if(arr.includes(winners[i][j])){
-				check++;
-			}else{
-				blank = winners[i][j];
-			}
-		}
-		if(check === 3){
-			alert(who + ' wins!');
-		}
-	}
-}
-
+//AI makes its move
 function aiMove(){
-	var moveAI = available[0];
+	var moveAI = 0;
+	if(checkWin(ai.sort(), 'AI')){
+		moveAI = blank;
+	}else if(checkWin(player.sort(), 'Player')){
+		moveAI = blank;
+	}else{
+		moveAI = available[0];
+	}
 	$('#' + moveAI).html('O');
 	ai.push(Number(moveAI));
 	checkWin(ai.sort(), 'AI');
 	available.splice(available.indexOf(Number(moveAI)), 1);
+}
+
+//checking for winner or for good blank place to mark
+function checkWin(arr, who){
+	for(var i = 0; i < winners.length; i++){
+		var check = 0;
+		var x = null;
+		for(var j = 0; j < 3; j++){
+			if(arr.includes(winners[i][j])){
+				check++;
+			}else{
+				x = winners[i][j];
+			}
+		}
+		if(check === 3){
+			alert(who + ' wins!');
+			return null;
+		}else if(check === 2 && available.includes(x)){
+			blank = x;
+		}
+	}
+	return blank;
 }
