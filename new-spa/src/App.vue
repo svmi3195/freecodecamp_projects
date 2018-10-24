f88b623c3c9af9a4c3a14eb911eec0e70f7755ea
svmi3195 <target1983@gmail.com>
2018-10-24 22:33:14 +0300

    add header

--- new-spa/src/App.vue
+++ new-spa/src/App.vue
@@ -1,27 +1,40 @@
 <template>
   <div id="app">
+    <Header />
     <Main />
   </div>
 </template>
 
 <script>
 import Main from './components/Main.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    Main
+    Main,
+    Header
   }
 }
 </script>
 
 <style>
+html, body, #app {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+header {
+  background-color: green;
+  color: white;
+  padding: 5px 0;
 }
 </style>
---
+++ new-spa/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <h1>FCC Projects</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
