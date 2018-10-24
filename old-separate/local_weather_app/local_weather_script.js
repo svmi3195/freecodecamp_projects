d0498da323c3e8a7305fc93669234cd1c31365a5
svmi3195 <target1983@gmail.com>
2018-10-24 21:20:11 +0300

    change structure to add spa for new projects

---
+++ old-separate/local_weather_app/local_weather_script.js
@@ -0,0 +1,39 @@
+﻿$( document ).ready(go);
+
+$('#degsBtn').on('click', changeDegs);
+
+var myJSON = null;
+var degC = null;
+var degF = null;
+
+function go(){
+
+ $('#location').text("Loading location data...");
+ $('#weather').text("Make sure you're using http, not https!"); 
+  
+$.getJSON("http://ip-api.com/json/?callback=?",function(result){   
+  $('#location').text(result.city + ", " + result.countryCode);   
+getWeather(result.city + "," + result.countryCode);                                                            
+})}
+
+function getWeather(loc){
+
+ $('#weather').text("Loading weather data..."); 
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + loc + "&APPID=d6a50d743e455ef9a573d33c339c225b&units=metric",function(data){   
+myJSON = data;
+degC = Math.round(data.main.temp);   
+var myImg = "<img src=\"http://openweathermap.org/img/w/" + data.weather[0].icon + ".png\">"
+    $('#image').append(myImg);    $('#weather').text(Math.round(data.main.temp) + " °C, " + data.weather[0].main);
+$('#degsBtn').show();
+  });
+    }
+
+function changeDegs(){
+ if(myJSON !== null && degC !== null){
+   degF = degC*9/5+32;
+   degC = null; $('#weather').text(Math.round(degF) + " °F, " + myJSON.weather[0].main);
+     }else if(myJSON !== null && degF !== null){
+     degC = (degF-32)*5/9;  
+     degF = null;     $('#weather').text(Math.round(myJSON.main.temp) + " °C, " + myJSON.weather[0].main);  
+     } 
+} 
---
+++ old-separate/random_quote_machine/random_quote_script.js
@@ -0,0 +1,45 @@
+﻿$(document).ready();
+
+$('#quoteBtn').on('click', randQuote);
+$('#tweetBtn').on('click', tweetIt);
+
+function randQuote(){
+
+$.ajax({
+            url: 'http://quotes.stormconsultancy.co.uk/random.json?callback=my_method',
+            type: 'GET',
+            dataType: 'jsonp',
+            error: function(xhr, status, error) {
+                $('#quote').text("Error occured while trying to get the quote, sorry!");
+$('#author').text("");
+            },
+            success: function(json) {
+                $('#quote').text(json.quote);
+		$('#author').text(json.author);
+              
+            }
+ 
+});
+
+  var colors = ['#ff0000', '#ff8000', '#ffff00', '#80ff00', '#00ff00', '#00ff80', '#00ffff', '#0080ff', '#ff00ff', '#ff0080', '#ffffff', '#999999', '#a64dff', '#3399ff', '#009999', '#669999', '#cc99ff', '#ff6699', '#ffccff', '#ff6600', '#ccff66', '#33cc33', '#00ff99',
+               '#8080ff', '#99ccff'];
+  var rand = Math.floor((Math.random() * (colors.length))); 
+$('body').css('color', colors[rand]);
+ 
+}
+
+function tweetIt(){
+  
+  var loc = "x";
+    
+    var quote  = $('#quote').text() + " (" + $('#author').text()+ ")";
+
+
+  quote = encodeURIComponent(quote);
+
+  window.open('http://twitter.com/share?url=' + loc + '&text=' + quote + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+  
+}
+
+
+
