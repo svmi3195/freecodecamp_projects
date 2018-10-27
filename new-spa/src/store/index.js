f4d6a8e184ac6f2e5f7d3f81134f9a130946f38b
svmi3195 <target1983@gmail.com>
2018-10-27 20:24:58 +0300

    add vuex

---
+++ new-spa/src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    projects: [
+      {
+        name: "project 1"
+      },
+      {
+        name: "project 2"
+      },
+      {
+        name: "project 3"
+      }
+    ]
+  }
+})
