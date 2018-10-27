a503f95acb07876eb5fe9a1ba446b5d939bd1771
svmi3195 <target1983@gmail.com>
2018-10-27 20:49:37 +0300

    render projects list at main component

--- new-spa/src/components/Main.vue
+++ new-spa/src/components/Main.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>Welcome to FCC Projects SPA!</div>
+  <div>
+    <h2>Welcome to FCC Projects SPA!</h2>
+    <p>Projecs:</p>
+      <p
+        v-for="name in list"
+        v-bind:key="name"
+        > {{name}} </p>
+    </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      list: this.$store.getters.list
+    }
+  }
 }
 </script>
 
